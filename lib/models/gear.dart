16c547deed5fd6da7f06aa50e3529a82e4030f08
Chris Livingston <mrchrislivingston@gmail.com>
2026-07-06 14:05:07 -0600

    Extract Gear model to separate file

---
+++ lib/models/gear.dart
@@ -0,0 +1,13 @@
+class Gear {
+  final int number;
+  final String work;
+  final String rest;
+  final int intervals;
+
+  const Gear({
+    required this.number,
+    required this.work,
+    required this.rest,
+    required this.intervals,
+  });
+}
